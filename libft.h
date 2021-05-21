d506d0aad1d15ac6b17127f30ec952b3f0264a86
hennneh <hendrik.lehmann96@gmail.com>
2021-05-21 13:59:07 +0200

    everything

--- libft.h
+++ libft.h
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memset(void *s, int c, size_t n); //überarbeiten
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
